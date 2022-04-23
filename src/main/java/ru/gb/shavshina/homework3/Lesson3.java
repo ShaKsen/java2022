0effe0e87bcc74a49a666a47de4c86451d64f0dd
User <ksks10987@mail.ru>
2022-04-23 13:58:20 +0300

    Remake

--- src/main/java/ru/gb/shavshina/homework3/Lesson3.java
+++ src/main/java/ru/gb/shavshina/homework3/Lesson3.java
@@ -8,18 +8,17 @@ public class Lesson3 {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
                 arr[i] = 0;
-                System.out.printf("%2d", arr[i]);
-            } else {
+                            } else {
                 arr[i] = 1;
-                System.out.printf("%2d", arr[i]);
             }
+            System.out.printf("%2d", arr[i]);
         }
         System.out.println();
         System.out.println();
 //Вторая задача
-        int[] a = new int[101];
-        for (int i = 1; i < a.length; i++) {
-            a[i] = i;
+        int[] a = new int[100];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i + 1;
             System.out.print(a[i] + " ");
         }
         System.out.println();
@@ -34,12 +33,19 @@ public class Lesson3 {
         }
         System.out.println();
 //Четвёртая задача
-        int[][] c = new int[4][4];
-        c[0][0] = c[1][1] = c[2][2] = c[3][3] = 1;
-        c[3][0] = c[2][1] = c[1][2] = c[0][3] = 1;
-        for (int i = 0; i < 4; i++) {
+       int x = 9;
+        int[][] c = new int[x][x];
+
+        for (int i = 0; i < x; i++) {
             System.out.println();
-            for (int j = 0; j < 4; j++) {
+            for (int j = 0; j < x; j++) {
+
+              if (i == j || i + j == x - 1) {
+                  c[i][j] = 1;
+              }
+              else {
+                  c[i][j] = 0;
+              }
                 System.out.print(c[i][j] + " ");
             }
         }
